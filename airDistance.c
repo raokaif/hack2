5dcc67b9e63a5e530dc550343a2306294baefb7a
raokaif <kaifrao250@gmail.com>
2023-10-07 17:39:35 +0500

    evaluating formula

--- airDistance.c
+++ airDistance.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    float d, lat1, lat2, lon1, lon2, R = 6371 , pi = 3.14;
+    float d, lat1, lat2, lon1, lon2, R = 6371 , pi = 3.14, diff;
     printf("Enter the latitude of location A:\n ");
     scanf("%f",lat1);
     printf("Enter the latitude of location B:\n ");
@@ -18,6 +18,8 @@ int main()
         lat2 = lat2/180*pi;
         lon1 = lon1/180*pi;
         lon2 = lon2/180*pi;
+        diff = lon2 -lon1 ;
+        d = acos((sin(lat1)) * (sin(lat2)) + (cos(lat1)) * (cos(lat2)) * (cos(diff))) *R;
         
     }
     else{
