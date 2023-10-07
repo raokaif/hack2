3c260354ebf6e01d6363bd2b4f1e8275ed0f984f
raokaif <kaifrao250@gmail.com>
2023-10-07 17:13:47 +0500

    scanf for user input

--- airDistance.c
+++ airDistance.c
@@ -2,6 +2,15 @@
 int main()
 {
     int d, lat1, lat2, lon1, lon2, R = 6371;
+    printf("Enter the latitude of location A:\n ");
+    scanf("%d",lat1);
+    printf("Enter the latitude of location B:\n ");
+    scanf("%d",lat2);
+    printf("Enter the longitude of location A:\n ");
+    scanf("%d",lon1);
+    printf("Enter the longitude of location B:\n ");
+    scanf("%d",lon2);
+
     return 0;
 }
 
